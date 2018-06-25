7c8351b280bc9ee936ad5e2ed679ab5792762a2c
TekLab <teklab@noreply.gitgem.com>
2018-06-25 10:38:59 +0200

    'games/minecraft/start.sh' hinzufügen

---
+++ games/minecraft/start.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+while test $# -gt 0; do
+ case "$1" in
+ "-IP")
+    IP="$2"
+    shift;;
+ "-PORT")
+    PORT="$2"
+    shift ;;
+ "-PLAYER")
+    PLAYER="$2"
+    shift ;;
+ "-RAM")
+    RAM="$2"
+    shift ;;
+ esac
+ shift
+done
+
+grep server-ip= server.properties &>/dev/null
+if [[ $? -eq 0 ]]; then  # Vorhanden!
+  sed -i server.properties -e "s/^\(server-ip=\).*$/\1${IP}/"
+else  # Nicht vorhanden!
+  echo "server-ip=${IP}" >> server.properties
+fi
+
+grep server-port= server.properties &>/dev/null
+if [[ $? -eq 0 ]]; then  # Vorhanden!
+  sed -i server.properties -e "s/^\(server-port=\).*$/\1${PORT}/"
+else  # Nicht vorhanden!
+  echo "server-port=${PORT}" >> server.properties
+fi
+
+grep max-players= server.properties &>/dev/null
+if [[ $? -eq 0 ]]; then  # Vorhanden!
+  sed -i server.properties -e "s/^\(max-players=\).*$/\1${PLAYER}/"
+else  # Nicht vorhanden!
+  echo "max-players=${PLAYER}" >> server.properties
+fi
+
+java -Xmx"$RAM"M -Xms"$RAM"M -jar minecraft_server.jar nogui $IP $PORT
